e7bab0ca990773a3c569bc835d98182fda983399
Alien-Oculus <tronceptexodia@gmail.com>
2017-12-01 14:14:38 -0700

    Jordans silly boy made some HUD chnages

---
+++ ConceptVR/Assets/ChangeClock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeClock : MonoBehaviour {
+
+    HUDManager HUD;
+    public Leap.Unity.LeapHandController leapcontroller;
+    float cdTime;
+
+    // Use this for initialization exodia
+    void Start()
+    {
+        HUD = GameObject.Find("Managers").GetComponent<HUDManager>();
+        cdTime = HUD.getCooldownTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "bone3" || collision.gameObject.name == "bone2")
+        {
+            Leap.Unity.FingerModel finger = collision.gameObject.GetComponentInParent<Leap.Unity.FingerModel>();
+
+            cdTime = HUD.getCooldown();
+            if (Time.time > cdTime)
+            {
+                if (finger && finger.fingerType.ToString() == "TYPE_INDEX")
+                {
+                    // swaps clock from digital to analog
+                    HUD.changeClock();
+                    HUD.setCooldownTime(Time.time + HUD.getCooldown());
+                }
+            }
+        }
+    }
+}
