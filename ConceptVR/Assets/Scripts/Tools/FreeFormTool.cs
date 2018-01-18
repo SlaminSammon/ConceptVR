7366c85f0c260011bc66ab84f0ae29d38345b112
tronceptexodia@gmail.com <tronceptexodia@gmail.com>
2018-01-18 09:51:12 -0700

    started creating free form tool

---
+++ ConceptVR/Assets/Scripts/Tools/FreeFormTool.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeFormTool : Tool {
+	private Vector3 startPos;
+	private Vector3 endPos;
+	private List<Vector3> rightPoints;
+	private List<Vector3> leftPoints;
+
+	// Use this for initialization
+	void Start () {
+		rightPoints = new List<Vector3> ();
+		leftPoints = new List<Vector3> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
