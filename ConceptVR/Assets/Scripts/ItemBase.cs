8871a9345a2ae0a1e7a8bac5c2f8d446180128d2
Alien-Oculus <tronceptexodia@gmail.com>
2018-01-25 13:35:26 -0700

    adding itembase

--- ConceptVR/Assets/Scripts/ItemBase.cs
+++ ConceptVR/Assets/Scripts/ItemBase.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ItemBase : MonoBehaviour {
 
+    public List<GameObject> lights;
+    public int lightCount;
+
 	// Use this for initialization
 	void Start () {
-		
+        lightCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void addLight(GameObject newLight)
+    {
+        lights.Add(newLight);
+        lightCount++;
+    }
+
 }
--- ConceptVR/Assets/Scripts/Tools/LightTool.cs
+++ ConceptVR/Assets/Scripts/Tools/LightTool.cs
@@ -6,19 +6,25 @@ public class LightTool : Tool {
     
     public GameObject LightPrefab;
 
+    GameObject itembase;
+
 	// Use this for initialization
 	void Start () {
+        itembase = GameObject.Find("ItemBase");
         LightPrefab.GetComponent<Light>().color = LightPrefab.GetComponent<Material>().color;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        
     }
 
     public override void Tap(Vector3 position)
     {
         LightPrefab.transform.position = controllerPosition;
-        Instantiate(LightPrefab);
+        GameObject newLight = Instantiate(LightPrefab) as GameObject;
+        newLight.name = "GeneratedPointLight_" + itembase.GetComponent<ItemBase>().lightCount;
+        newLight.transform.parent = itembase.transform;
+        itembase.GetComponent<ItemBase>().addLight(newLight);
     }
 }
