2ea26135a01baa4ae576bc5368ffe0887cbdf0ed
Alien-Oculus <tronceptexodia@gmail.com>
2018-01-25 13:02:27 -0700

    adding light tool

---
+++ ConceptVR/Assets/Scripts/Tools/LightTool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTool : Tool {
+    
+    public GameObject LightPrefab;
+
+	// Use this for initialization
+	void Start () {
+        LightPrefab.GetComponent<Light>().color = LightPrefab.GetComponent<Material>().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public override void Tap(Vector3 position)
+    {
+        LightPrefab.transform.position = controllerPosition;
+        Instantiate(LightPrefab);
+    }
+}
