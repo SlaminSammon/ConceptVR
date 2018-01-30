3172a7f9928bccb5dbf77e1671546b7dabf68e76
Chris Renden <renden.christopher@gmail.com>
2018-01-30 12:22:54 -0700

    DCG Synchronization stuff

---
+++ ConceptVR/Assets/Scripts/NetPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetPlayer : NetworkBehaviour {
+    public static NetPlayer local;
+
+    public int playerID;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
