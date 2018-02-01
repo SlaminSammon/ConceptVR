6560ec5038dd38087dacfaabc61c61f6158bfe8f
DESKTOP-199V31T\tronc <tronceptexodia@gmail.com>
2018-02-01 14:01:28 -0700

    Slider is operational

--- ConceptVR/Assets/Scripts/HUD/HUDSlider.cs
+++ ConceptVR/Assets/Scripts/HUD/HUDSlider.cs
@@ -6,6 +6,8 @@ public class HUDSlider : HUDButton {
     public float friction;
     public Vector3 positiveRange;
     public float defaultValue;
+    public List<float> snapPoints;
+    public float snapRange;
 
     [HideInInspector] public float value;
     float targetValue;
@@ -24,10 +26,26 @@ public class HUDSlider : HUDButton {
         base.Update();
         if (touching)
         {
-            localPressPos = transform.worldToLocalMatrix * fingerTip.transform.position;
-            targetValue = Vector3.Project(localPressPos - positiveRange, positiveRange).magnitude;
-            Debug.Log(targetValue);
+            localPressPos = transform.InverseTransformPoint(fingerTip.transform.position);
+            targetValue = Vector3.Project(localPressPos + positiveRange, positiveRange).magnitude / positiveRange.magnitude / 2;
+            if (Vector3.Dot(localPressPos + positiveRange, positiveRange) < 0)
+                targetValue = 0;
             targetValue = Mathf.Clamp01(targetValue);
+
+            int nearestSnap = -1;
+            float nearestDist = snapRange;
+            for(int i = 0; i < snapPoints.Count; ++i)
+            {
+                float diff = Mathf.Abs(targetValue - snapPoints[i]);
+                if (diff < nearestDist)
+                {
+                    nearestDist = diff;
+                    nearestSnap = i;
+                }
+            }
+
+            if (nearestSnap != -1)
+                targetValue = snapPoints[nearestSnap];
         }
         value = (value * friction + targetValue) / (1+friction);
         sliderObject.transform.localPosition = (value * 2 - 1) * positiveRange;
