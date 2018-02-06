6141449dc0be8f0e09289da84e97f6cc4eabf5ec
Alien-Oculus <tronceptexodia@gmail.com>
2018-02-06 15:09:34 -0700

    material tool and more!

---
+++ ConceptVR/Assets/Scripts/HUD/ChangeMaterialButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialButton : HUDButton {
+
+    public Material material;
+    public Tool materialTool;
+	// Use this for initialization
+	void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        base.Update();
+	}
+
+    public override void OnPress()
+    {
+        materialTool.GetComponent<MaterialTool>().changeMaterial(material);
+        base.OnPress();
+    }
+}
