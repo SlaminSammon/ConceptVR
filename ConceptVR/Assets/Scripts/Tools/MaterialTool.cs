26163b1f3cf6ed55fe24758fbb3a0056bc0364a4
DESKTOP-199V31T\tronc <tronceptexodia@gmail.com>
2018-02-20 17:25:03 -0700

    Actually the fuck fixed it

--- ConceptVR/Assets/Scripts/Tools/MaterialTool.cs
+++ ConceptVR/Assets/Scripts/Tools/MaterialTool.cs
@@ -27,7 +27,8 @@ public class MaterialTool : Tool {
             if (!DCGBase.matList.Contains(newDCGMat))   //make sure mat is in the list
                 DCGBase.matList.Add(newDCGMat);
 
-            //nearestFace.mat.RemoveFace(nearestFace);    //unset previous mat
+            if (nearestFace.mat != null)
+                nearestFace.mat.RemoveFace(nearestFace);    //unset previous mat
             newDCGMat.AddFace(nearestFace); //set new mat
             return true;
         }
