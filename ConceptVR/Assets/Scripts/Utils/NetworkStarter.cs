148851b6a31ed5d0425210bc745b909ea82ae302
DESKTOP-199V31T\tronc <tronceptexodia@gmail.com>
2018-02-22 09:59:08 -0700

    NetworkStarter

---
+++ ConceptVR/Assets/Scripts/Utils/NetworkStarter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkStarter : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("NetworkManager").GetComponent<NetworkManager>().StartHost();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
