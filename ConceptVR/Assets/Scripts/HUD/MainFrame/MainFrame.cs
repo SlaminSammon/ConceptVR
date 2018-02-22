3eacf8511e376b5718aeccc8d992a45c288dd6a1
DESKTOP-L0SBVMF\Troncept <tronceptexodia@gmail.com>
2018-02-22 10:05:58 -0700

    no message

---
+++ ConceptVR/Assets/Scripts/HUD/MainFrame/MainFrame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainFrame : HUDFrame {
+
+    const float SCALE = 10;
+    HUDSlider scaleSlider;
+    public GameObject LMHeadMountedRig;
+
+	// Use this for initialization
+	void Start () {
+        scaleSlider = this.gameObject.transform.Find("ScaleSlider").GetComponent<HUDSlider>();
+
+        // base.Start();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //transform.Find("Managers").GetComponent<SettingsManager>().scaleSlider = new Vector3(scaleSlider.value * SCALE + 1f, scaleSlider.value * SCALE + 1f, scaleSlider.value * SCALE + 1f);
+        LMHeadMountedRig.transform.localScale = new Vector3(scaleSlider.value * SCALE + 1f, scaleSlider.value * SCALE + 1f, scaleSlider.value * SCALE + 1f);
+        // base.Update();
+    }
+}
