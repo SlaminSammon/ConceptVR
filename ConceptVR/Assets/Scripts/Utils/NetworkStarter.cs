a1a732beac3d0fd57c61fac67458dca2c1c25f20
DESKTOP-199V31T\tronc <tronceptexodia@gmail.com>
2018-02-27 14:51:25 -0700

    last push

--- ConceptVR/Assets/Scripts/Utils/NetworkStarter.cs
+++ ConceptVR/Assets/Scripts/Utils/NetworkStarter.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 public class NetworkStarter : MonoBehaviour {
     public bool startHost;
     private NetworkManager netManager;
+    bool connecting = false;
 	// Use this for initialization
 	void Start () {
         netManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
@@ -18,10 +19,15 @@ public class NetworkStarter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!netManager.isNetworkActive)
+        if (!netManager.isNetworkActive && !connecting && startHost)
         {
+            Debug.Log("Starting new Host");
             netManager.networkAddress = "localhost";
             netManager.StartHost();
+        }
+        if(netManager.isNetworkActive && connecting && Network.isClient)
+        {
+            connecting = false;
         }
     }
     public void connectToHost(string netAddress, int netPort)
@@ -30,5 +36,6 @@ public class NetworkStarter : MonoBehaviour {
         netManager.networkAddress = netAddress;
         netManager.networkPort = netPort;
         netManager.StartClient();
+        connecting = true;
     }
 }
