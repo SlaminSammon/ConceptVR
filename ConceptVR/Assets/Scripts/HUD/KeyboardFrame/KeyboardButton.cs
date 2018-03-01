37b4082b51734b2fb463fdef3e8a74a17e58d73b
DESKTOP-L0SBVMF\Troncept <tronceptexodia@gmail.com>
2018-03-01 14:33:34 -0700

    working keyboard!!!

---
+++ ConceptVR/Assets/Scripts/HUD/KeyboardFrame/KeyboardButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardButton : HUDButton {
+
+    public string text;
+
+	// Use this for initialization
+	void Start () {
+        base.Start();
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+
+    public override void OnPress()
+    {
+        if (text == "backspace")
+        {
+            string textBoxText = transform.parent.Find("Textbox").GetComponent<TextMesh>().text;
+            if (textBoxText != "")
+            {
+                // remove last character from string
+                textBoxText = textBoxText.Substring(0, (textBoxText.Length - 1));
+                transform.parent.Find("Textbox").GetComponent<TextMesh>().text = textBoxText;
+            }
+        }else if (text == "enter")
+        {
+            transform.parent.Find("Textbox").GetComponent<TextMesh>().text += '\n';
+        }
+        else
+        {
+            transform.parent.Find("Textbox").GetComponent<TextMesh>().text += text;
+        }
+        base.OnPress();
+    }
+
+}
---
+++ ConceptVR/Assets/Scripts/HUD/KeyboardFrame/PlaceTextButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTextButton : HUDButton {
+
+    public GameObject textBox;
+	// Use this for initialization
+	void Start () {
+        base.Start();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        base.Update();
+	}
+
+    public override void OnPress()
+    {
+        GameObject generatedText = GameObject.Instantiate(textBox);
+        generatedText.name = "GeneratedText - " + generatedText.GetComponent<TextMesh>().text;
+        generatedText.transform.position = textBox.transform.position;
+        generatedText.transform.rotation = textBox.transform.rotation;
+        // clear the current text in the textbox
+        textBox.GetComponent<TextMesh>().text = "";
+
+        base.OnPress();
+    }
+
+
+}
