bc0ee4d4e1deb72a9930e4b49fc6110b72a27634
DESKTOP-199V31T\tronc <tronceptexodia@gmail.com>
2018-03-08 11:36:34 -0700

    red underscore functionality

--- ConceptVR/Assets/Scripts/HUD/KeyboardFrame/PlaceTextButton.cs
+++ ConceptVR/Assets/Scripts/HUD/KeyboardFrame/PlaceTextButton.cs
@@ -20,12 +20,17 @@ public class PlaceTextButton : HUDButton {
     {
         if (textBox.GetComponent<TextMesh>().text != null)
         {
+            // remove red underscore before generating the text
+            string textBoxText = textBox.GetComponent<TextMesh>().text;
+            textBoxText = textBoxText.Substring(0, (textBoxText.Length - 20));
+            textBox.GetComponent<TextMesh>().text = textBoxText;
+
             GameObject go = Instantiate(ItemBase.itemBase.TextPrefab, textBox.transform.position, textBox.transform.rotation);
             go.GetComponent<TextItem>().updateText(textBox.GetComponent<TextMesh>().text);
             go.transform.localScale = textBox.transform.localScale *1.25f;
             ItemBase.Spawn(go);
             // clear the current text in the textbox
-            textBox.GetComponent<TextMesh>().text = "";
+            textBox.GetComponent<TextMesh>().text = "<color=red>_</color>";
         }
 
 
